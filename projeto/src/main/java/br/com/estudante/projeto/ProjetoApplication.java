a4dcf05458a73cdf0033b1e236e94bbe275411ff
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2024-09-27 16:11:32 -0300

    three

--- projeto/src/main/java/br/com/estudante/projeto/ProjetoApplication.java
+++ projeto/src/main/java/br/com/estudante/projeto/ProjetoApplication.java
@@ -3,6 +3,7 @@ package br.com.estudante.projeto;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.estudante.projeto.principal.Principal;
 
 @SpringBootApplication
 public class ProjetoApplication implements CommandLineRunner {
@@ -13,7 +14,9 @@ public class ProjetoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("\n\n\nRafa");
+		System.out.println("\n\n\nRafael faça cada vez melhor S2\n\n");
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
