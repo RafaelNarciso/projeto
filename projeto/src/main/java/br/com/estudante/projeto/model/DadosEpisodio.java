624fe837eb1a453c760cf758d44a5a068b4bf9cf
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2024-09-27 16:03:30 -0300

    two

---
+++ projeto/src/main/java/br/com/estudante/projeto/model/DadosEpisodio.java
@@ -0,0 +1,11 @@
+package br.com.estudante.projeto.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+        @JsonAlias("Episode") Integer numero,
+        @JsonAlias("imdbRating") String avaliacao,
+        @JsonAlias("Released") String dataLancamento) {
+}
